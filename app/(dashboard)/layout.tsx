bd19df0afb7d9d4286c4467ec2f80dff1a96e284
Larin Nikita <l_nikita2000@mail.ru>
2025-03-23 20:33:33 +0700

    setup clerk. Add user button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
 import DesktopSidebar from '@/components/Sidebar';
 import { Separator } from '@/components/ui/separator';
 import BreadcrumbHeader from '@/components/Breadcrumb';
@@ -16,8 +18,11 @@ function Layout({ children }: LayoutProps) {
       <div className="flex min-h-screen flex-1 flex-col">
         <header className="container flex h-[50px] items-center justify-between px-6 py-4">
           <BreadcrumbHeader />
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-2">
             <ModeToggle />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
         </header>
         <Separator />
